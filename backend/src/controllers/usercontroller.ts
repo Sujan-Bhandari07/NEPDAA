fdb7b05c9e1f2fc82b70c3e961a4cd747dbf8fb1
Sujan Bhandari <sujanbhandari474@gmail.com>
2026-07-31 11:51:58 +0545

    Update usercontroller.ts

--- backend/src/controllers/usercontroller.ts
+++ backend/src/controllers/usercontroller.ts
@@ -68,11 +68,10 @@ const registeruser = async (
     }
 
     const token = gettoken(user._id);
-    res.cookie("token", token, {
+ res.cookie("token", token, {
   httpOnly: true,
   secure: true,
-  sameSite: "lax", // can relax this now since it's same-site
-  domain: ".nepdaa.com",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
@@ -125,11 +124,10 @@ const login = async (
     }
     const token = gettoken(existuser._id);
     if (token) {
-     res.cookie("token", token, {
+   res.cookie("token", token, {
   httpOnly: true,
   secure: true,
-  sameSite: "lax", // can relax this now since it's same-site
-  domain: ".nepdaa.com",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 });
     const a = await User.findOne({email:email}).select("-password")
